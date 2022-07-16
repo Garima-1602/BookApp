bfa38e502d29762e2a3aeae25abf39db51ce5d5e
Garima <garima89077@gmail.com>
2022-07-16 13:47:23 +0530

    adapterclass

--- app/src/main/java/fragment/DashboardFragment.kt
+++ app/src/main/java/fragment/DashboardFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.bookapp.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
