8620620d14f89bb9d485b8d6def5edb93259e30b
Garima <garima89077@gmail.com>
2022-07-17 15:37:55 +0530

    checking internet connectivity

--- app/src/main/java/util/ConnectionManager.kt
+++ app/src/main/java/util/ConnectionManager.kt
@@ -1,4 +1,21 @@
 package util
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+
 class ConnectionManager {
+    fun checkConnectivity(context: Context):Boolean{
+        //this gives us information about currently active network
+    val connectivityManager=context.getSystemService(Context.CONNECTIVITY_SERVICE)as ConnectivityManager
+        val activeNetwork:NetworkInfo?=connectivityManager.activeNetworkInfo
+        if(activeNetwork?.isConnected!=null)
+        {
+            return activeNetwork.isConnected
+        }
+        else
+        {
+            return false
+        }
+    }
 }
